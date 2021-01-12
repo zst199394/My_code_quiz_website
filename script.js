0343482c66dfed2cd97c0fe43bc392a988cc12ba
shuting <zst9394@outlook.com>
2021-01-12 13:30:59 -0500

    js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Getting all required elements
+var button =document.getElementById("start-btn");
+var timer =document .getElementById("timer");
+var quizContainer =document.getElementById("quiz");
+
+
+
+/* create function
+THEN a timer starts and I am presented with a question*/
+function startGame(){
+
+}
+/*WHEN I answer a question
+THEN I am presented with another question
+
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over*/
+// THEN I can save my initials and score
+
